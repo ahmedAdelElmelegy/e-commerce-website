66b8e4d5b89346656169cb08cc1864840aad546b
ahmed adel <ahmedadelelmelegy2@gmail.com>
2024-08-08 04:53:02 +0300

    edit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cors_proxy = require('cors-anywhere');
 const host = 'localhost';
-const port = 8080;
+const port =process.env.PORT|| 8080;
 
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
